8989b0f8fbb779946c28de8cd585f0e4927183ef
Oleg <new@MacBook-Pro-4.local>
2021-06-05 02:17:36 +0300

    function for a table creation

--- front/src/components/Board/Board.tsx
+++ front/src/components/Board/Board.tsx
@@ -1,30 +1,33 @@
-import React, { memo } from 'react';
+import React, { memo } from "react";
 
-import Square from './Square/Square';
-import { AssetProps } from '../Assets/Asset';
+import Square from "./Square/Square";
+import { AssetProps } from "../Assets/Asset";
 
 export interface SquareState {
-  x:number,
-  y:number,
-  isEmpty:boolean,
-  asset?:AssetProps
+  x: number;
+  y: number;
+  isEmpty: boolean;
+  asset?: AssetProps;
 }
 function Board() {
-  function createBoard(countX:number, countY:number):SquareState[] {
+  function createBoard(countX: number, countY: number): SquareState[] {
     const board = [];
-    for (let i = 0; i < countX; i += 1) {
-      const squareX = Object({ isEmpty: true, x: 0, y: i });
-      i = 0;
-      for (let j = 0; j < countY; j += 1) {
-        const squareY = Object({ isEmpty: true, x: j, y: 0 });
-        board.push(squareX, squareY);
+    let i = 0,
+      j = 0;
+    while (i < countY) {
+      const coordinates = Object({ isEmpty: true, x: j, y: i });
+      i++;
+      if (i === countY && j < countX - 1) {
+        i = 0;
+        j++;
       }
+      board.push(coordinates);
     }
     return board;
   }
 
   const squaresState = createBoard(5, 5);
-  console.log(squaresState, 'squaresState');
+  console.log(squaresState, "squaresState");
 
   return (
     <>
